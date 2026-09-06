f263cc5375655ba98186f1a9d218ec93cfa0668b
Ekalavya Latchireddi <ekalvyalatchireddi123@gmail.com>
2026-09-06 15:28:43 +0530

    enhancement in recipe access update ui pop and notifcations bars

--- frontEnd/src/main.tsx
+++ frontEnd/src/main.tsx
@@ -1,12 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './app/App'
+import { NotificationProvider } from './shared/components/notifications/NotificationProvider'
 import './App.css'
 
 import './styles/global.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <NotificationProvider>
+      <App />
+    </NotificationProvider>
   </React.StrictMode>,
 )
